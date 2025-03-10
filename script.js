00b5f91264defba2dbbc7a40c140510a0945181d
Frank Rimiru <rimirufrank@gmail.com>
2025-03-10 18:57:01 +0300

    finished all

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function toggleMenu() {
   const menu = document.querySelector('.menu-links');
   const icon = document.querySelector('.hamburger-icon');
-menu.classList.toggle('open')
-icon.classList.toggle('open')
 
+console.log('toggle menu')
+
+  menu.classList.toggle('open')
+  console.log('menu')
+  icon.classList.toggle('open')
 }
